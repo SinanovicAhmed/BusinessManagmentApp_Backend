af758ea447244f80c3cfc3ef0924556404505013
SinanovicAhmed <ahmedsinanovic20@gmail.com>
2023-01-02 19:11:36 +0100

    initial

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  extends: ["plugin:@typescript-eslint/recommended"],
+  parseOptions: {
+    ecmaVersion: 2018,
+    sourceType: module,
+  },
+  rules: {},
+};
