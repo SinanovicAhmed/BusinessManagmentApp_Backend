ee232b84285286ba4b406f55072e7adc20882a2c
SinanovicAhmed <ahmedsinanovic20@gmail.com>
2023-07-08 13:07:48 +0200

    Material model updated

--- src/models/materialModel.js
+++ src/models/materialModel.js
@@ -1,11 +1,6 @@
 const mongoose = require("mongoose");
 
 const materialSchema = mongoose.Schema({
-  supplier_id: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: [true, "Supplier id is missing!"],
-    ref: "Supplier",
-  },
   material_name: {
     type: String,
     required: [true, "Material name is missing"],
@@ -14,17 +9,13 @@ const materialSchema = mongoose.Schema({
     type: Number,
     required: [true, "Minimal quantity is missing"],
   },
-  price: {
+  quantity: {
     type: Number,
-    required: [true, "Price is missing"],
+    required: [true, "Quantity is missing"],
   },
   unit_of_measure: {
     type: String,
     required: [true, "Unit of measure is missing"],
-  },
-  in_use: {
-    type: Boolean,
-    required: [true, "In use is missing"],
   },
 });
 
