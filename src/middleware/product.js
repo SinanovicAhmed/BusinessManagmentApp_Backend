8bc0b2b0e10edb36bf82bae029209b27be8e4040
SinanovicAhmed <ahmedsinanovic20@gmail.com>
2023-10-05 12:37:54 +0200

    Creating products. Reducing material quantity based on materials spent on creationg of the product

--- src/middleware/product.js
+++ src/middleware/product.js
@@ -1,5 +1,6 @@
 const jwt = require("../middleware/JWT");
 const Product = require("../models/productModel");
+const Material = require("../models/materialModel");
 
 exports.getAllProducts = async (req, res) => {
   try {
@@ -17,5 +18,38 @@ exports.addProduct = async (req, res) => {
     res.json({ message: "Product is successfully added" });
   } catch (err) {
     res.status(500).json({ message: err.message });
+  }
+};
+
+exports.createProduct = async (req, res) => {
+  const { materials_needed, product_id } = req.body;
+
+  try {
+    const materialIds = materials_needed.map((material) => material.material_id);
+    const existingMaterials = await Material.find({ _id: { $in: materialIds } });
+
+    // Check if all materials exist
+    if (existingMaterials.length !== materials_needed.length) {
+      return res.status(404).json({ message: "One or more materials not found." });
+    }
+    //update material quantity after checking if they exist
+    for (const material of materials_needed) {
+      const { material_id, quantity } = material;
+
+      const existingMaterial = await Material.findOne({ _id: material_id });
+      if (existingMaterial.quantity >= quantity) {
+        existingMaterial.quantity -= quantity;
+      } else {
+        return res.status(422).json({ message: "You dont have enough " + existingMaterial.material_name });
+      }
+      await existingMaterial.save();
+    }
+
+    const createdProduct = await Product.findOne({ _id: product_id });
+    createdProduct.quantity++;
+    await createdProduct.save();
+    res.status(200).json({ message: "You succesfully created product." });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
   }
 };
--- src/routes/productRoute.js
+++ src/routes/productRoute.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const jwt = require("../middleware/JWT");
-const { getAllProducts, addProduct } = require("../middleware/product");
+const { getAllProducts, addProduct, createProduct } = require("../middleware/product");
 
 router.get("/get-all", getAllProducts);
 router.post("/add-product", addProduct);
+router.patch("/create-product", createProduct);
 module.exports = router;
